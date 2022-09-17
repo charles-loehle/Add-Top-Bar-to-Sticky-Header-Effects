1829cad2225f04deea6aec0aef30f5e72c19ed4b
Charles Loehle <clguitar1@gmail.com>
2022-09-17 08:59:08 -0400

    initial commit

---
+++ elementor-sticky-header-with-top-bar.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * Plugin Name:       Add Top Bar to Sticky Header Effects
+ * Plugin URI:        https://clwebdevelopment.com/
+ * Description:       Integrates with Sticky Header Effects to animate a top bar smoothly with the main header.
+ * Version:           1.0.0
+ * Requires PHP:      7.2
+ * Author:            Charles Loehle
+ * Author URI:        https://clwebdevelopment.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        elementor-sticky-header-with-top-bar
+ * Text Domain:       elementor-sticky-header-with-top-bar
+ * Domain Path:       /languages
+ */
